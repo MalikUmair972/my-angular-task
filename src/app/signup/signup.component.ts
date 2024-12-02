915e5da3fea13ae8b07319941cd0b7c0002a5c2c
MalikUmair972 <mu501362@gmail.com>
2024-12-02 16:29:15 +0500

    Sign Up Form Component

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+}
+
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+  email: string = '';
+  password: string = '';
+  agree: boolean = false;
+  submitAttempted: boolean = false;
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8), 
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+
+  onSubmit() {
+    this.submitAttempted = true;
+
+    if (this.emailFormControl.valid && this.passwordControl.valid) {
+      console.log('Form Submitted', {
+        email: this.email,
+        password: this.password,
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
