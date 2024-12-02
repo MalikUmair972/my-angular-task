72b1970624bfe2efb3ebd4dfe3aa6e90a2050e5b
MalikUmair972 <mu501362@gmail.com>
2024-12-02 16:28:25 +0500

    Manage Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,52 @@
 import { Component } from '@angular/core';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  providers: [provideNativeDateAdapter()],
 })
 export class AppComponent {
   title = 'my-angular-task';
 
   email: string = '';
-  lastName: string = '';
+  password: string = '';
   agree: boolean = false;
   submitAttempted: boolean = false;
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8), 
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+
+  onSubmit() {
+    this.submitAttempted = true;
+
+    if (this.emailFormControl.valid && this.passwordControl.valid) {
+      console.log('Form Submitted', {
+        email: this.email,
+        password: this.password,
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,45 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { ValidationErrors } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
     
   ],
   providers: [
-    provideClientHydration()
+
+
   ],
   bootstrap: [AppComponent]
 })
